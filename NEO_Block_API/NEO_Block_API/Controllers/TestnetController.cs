a8c6a08de565c4951250e380553e0419ab9f06ca
QingmingZi <qingmingzi@hotmail.com>
2017-12-29 11:03:49 +0800

    兼容txid大小写和不带0x情况；utxo默认返回未消费，也支持全部返回；增加参数错误、无数据2种错误返回。

--- NEO_Block_API/NEO_Block_API/Controllers/TestnetController.cs
+++ NEO_Block_API/NEO_Block_API/Controllers/TestnetController.cs
@@ -16,32 +16,76 @@ namespace NEO_Block_API.Controllers
     {
         mongoHelper mh = new mongoHelper();
 
+        private string formatTxid(string txid)
+        {
+            string result = txid.ToLower();
+            if (result.Length == 64) {
+                result = "0x" + result;
+            }
+
+            return result;
+        }
+
         private JsonResult getRes(JsonRPCrequest req)
         {
             JArray result = new JArray();
             string findFliter = string.Empty;
-            switch (req.method)
+            try {
+                switch (req.method)
+                {
+                    case "getblock":
+                        findFliter = "{index:" + req.@params[0] + "}";
+                        result = mh.GetData(mh.mongodbConnStr_testnet, mh.mongodbDatabase_testnet, "block", findFliter);
+                        break;
+                    case "gettransaction":
+                        findFliter = "{txid:'" + formatTxid((string)req.@params[0]) + "'}";
+                        result = mh.GetData(mh.mongodbConnStr_testnet, mh.mongodbDatabase_testnet, "tx", findFliter);
+                        break;
+                    case "getfulllog":
+                        findFliter = "{txid:'" + formatTxid((string)req.@params[0]) + "'}";
+                        result = mh.GetData(mh.mongodbConnStr_testnet, mh.mongodbDatabase_testnet, "fulllog", findFliter);
+                        break;
+                    case "getnotify":
+                        findFliter = "{txid:'" + formatTxid((string)req.@params[0]) + "'}";
+                        result = mh.GetData(mh.mongodbConnStr_testnet, mh.mongodbDatabase_testnet, "notify", findFliter);
+                        break;
+                    case "getutxo":
+                        if (req.@params.Count() == 1)
+                        {
+                            findFliter = "{addr:'" + req.@params[0] + "',vinTx:null}";
+                        };
+                        if (req.@params.Count() == 2)
+                        {
+                            if ((Int64)req.@params[1] == 1)
+                            {
+                                findFliter = "{addr:'" + req.@params[0] + "'}";
+                            }
+                        }
+                        result = mh.GetData(mh.mongodbConnStr_testnet, mh.mongodbDatabase_testnet, "utxo", findFliter);
+                        break;
+                }
+                if (result.Count == 0) {
+                    JsonPRCresponse_Error resE = new JsonPRCresponse_Error();
+                    resE.jsonrpc = "2.0";
+                    resE.id = 0;
+                    resE.error.code = -1;
+                    resE.error.message = "No Data";
+                    resE.error.data = "Data does not exist";
+
+                    return Json(resE);
+                }
+            }
+            catch (Exception e)
             {
-                case "getblock":
-                    findFliter = "{index:" + req.@params[0] + "}";
-                    result = mh.GetData(mh.mongodbConnStr_testnet, mh.mongodbDatabase_testnet, "block", findFliter);
-                    break;
-                case "gettransaction":
-                    findFliter = "{txid:'" + req.@params[0] + "'}";
-                    result = mh.GetData(mh.mongodbConnStr_testnet, mh.mongodbDatabase_testnet, "tx", findFliter);
-                    break;
-                case "getfulllog":
-                    findFliter = "{txid:'" + req.@params[0] + "'}";
-                    result = mh.GetData(mh.mongodbConnStr_testnet, mh.mongodbDatabase_testnet, "fulllog", findFliter);
-                    break;
-                case "getnotify":
-                    findFliter = "{txid:'" + req.@params[0] + "'}";
-                    result = mh.GetData(mh.mongodbConnStr_testnet, mh.mongodbDatabase_testnet, "notify", findFliter);
-                    break;
-                case "getutxo":
-                    findFliter = "{Addr:'" + req.@params[0] + "'}";
-                    result = mh.GetData(mh.mongodbConnStr_testnet, mh.mongodbDatabase_testnet, "utxo", findFliter);
-                    break;
+                JsonPRCresponse_Error resE = new JsonPRCresponse_Error();
+                resE.jsonrpc = "2.0";
+                resE.id = 0;
+                resE.error.code = -100;
+                resE.error.message = "Parameter Error";
+                resE.error.data = e.Message;
+
+                return Json(resE);
+
             }
 
             JsonPRCresponse res = new JsonPRCresponse();
@@ -74,7 +118,7 @@ namespace NEO_Block_API.Controllers
                 resE.jsonrpc = "2.0";
                 resE.id = 0;
                 resE.error.code = -100;
-                resE.error.message = "Parameter error";
+                resE.error.message = "Parameter Error";
                 resE.error.data = e.Message;
 
                 return Json(resE);
