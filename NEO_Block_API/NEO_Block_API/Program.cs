76c3fa91f1bc83f60c38853233273657ea001400
qingmingzi <qingmingzi@hotmail.com>
2018-06-14 14:50:19 +0800

    配置json可以增加serverPort节点配置服务端口号，灵活变更api http服务端口

--- NEO_Block_API/NEO_Block_API/Program.cs
+++ NEO_Block_API/NEO_Block_API/Program.cs
@@ -18,12 +18,34 @@ namespace NEO_Block_API
             BuildWebHost(args).Run();
         }
 
+        private static int getServerPort() {
+            int serverPort = 0;
+
+            //尝试从配置文件读取，失败则默认82
+            try
+            {
+                var config = new ConfigurationBuilder()
+               .AddInMemoryCollection()    //将配置文件的数据加载到内存中
+               .SetBasePath(Directory.GetCurrentDirectory())   //指定配置文件所在的目录
+               .AddJsonFile("mongodbsettings.json", optional: true, reloadOnChange: true)  //指定加载的配置文件
+               .Build();    //编译成对象        
+
+                serverPort = int.Parse(config["serverPort"].ToString());
+            }
+            catch
+            {
+                serverPort = 82;
+            }
+
+            return serverPort;            
+        }
+
         public static IWebHost BuildWebHost(string[] args) =>
              WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
                 .UseKestrel(options =>
                 {
-                    options.Listen(IPAddress.Any, 82);
+                    options.Listen(IPAddress.Any, getServerPort());
                     //options.Listen(IPAddress.Loopback, 5001, listenOptions =>
                     //{
                     //    listenOptions.UseHttps("testCert.pfx", "testPassword");
