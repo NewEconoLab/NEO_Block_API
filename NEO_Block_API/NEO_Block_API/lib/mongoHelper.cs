fde1ea768cbd8ea5eb59362b5acca5dcfb1e064f
qingmingzi <matrix3345@hotmail.com>
2018-10-11 09:33:50 +0800

    配置文件读取模式改为不跟踪

--- NEO_Block_API/NEO_Block_API/lib/mongoHelper.cs
+++ NEO_Block_API/NEO_Block_API/lib/mongoHelper.cs
@@ -28,7 +28,7 @@ namespace NEO_Block_API.lib
             var config = new ConfigurationBuilder()
                 .AddInMemoryCollection()    //将配置文件的数据加载到内存中
                 .SetBasePath(System.IO.Directory.GetCurrentDirectory())   //指定配置文件所在的目录
-                .AddJsonFile("mongodbsettings.json", optional: true, reloadOnChange: true)  //指定加载的配置文件
+                .AddJsonFile("mongodbsettings.json", optional: true, reloadOnChange: false)  //指定加载的配置文件
                 .Build();    //编译成对象  
             mongodbConnStr_testnet = config["mongodbConnStr_testnet"];
             mongodbDatabase_testnet = config["mongodbDatabase_testnet"];
